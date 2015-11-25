121e0b22ae6588d15324f08356b6eb88198b1aad
Anton Nonko <anton.nonko@gmail.com>
2015-11-25 10:11:41 +0300

    Macros to work with template parameters.

---
+++ string_facade/string_facade_fwd.hpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2015 Anton Nonko
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef STRING_FACADE_FWD_HPP
+#define STRING_FACADE_FWD_HPP
+
+#include "detail/string_facade/preprocessor.hpp"
+
+/**
+ * @todo Write documentation.
+ */
+#define STRING_FACADE_PARAMS\
+  ((class) (CategoryOrTraversal) (0) (               ) (                                                                 ))\
+  ((int  ) (Flags              ) (0) (               ) (                                                                 ))\
+  ((class) (Iterator           ) (0) (               ) (                                                                 ))\
+  ((class) (ConstIterator      ) (0) (               ) (= typename detail::string_facade::deduce_const_iterator<Iterator>))\
+  ((class) (Char               ) (1) (value_type     ) (= typename ::boost::iterator_value<Iterator>::type               ))\
+  ((class) (CharTraits         ) (1) (traits_type    ) (= ::std::char_traits<Char>                                       ))\
+  ((class) (Size               ) (1) (size_type      ) (= ::std::size_t                                                  ))\
+  ((class) (Difference         ) (1) (difference_type) (= ::std::ptrdiff_t                                               ))\
+  ((class) (Pointer            ) (1) (pointer        ) (= typename ::boost::iterator_pointer<Iterator>::type             ))\
+  ((class) (ConstPointer       ) (1) (const_pointer  ) (= typename ::boost::iterator_pointer<ConstIterator>::type        ))\
+  ((class) (Reference          ) (1) (reference      ) (= typename ::boost::iterator_reference<Iterator>::type           ))\
+  ((class) (ConstReference     ) (1) (const_reference) (= typename ::boost::iterator_reference<ConstIterator>::type      ))\
+
+/**
+ * @todo Write documentation.
+ */
+#define STRING_FACADE_PARAMS_DECLARE(...)\
+  DETAIL_STRING_FACADE_PARAMS_ENUM(\
+    DETAIL_STRING_FACADE_PARAMS_REMOVE((None)__VA_ARGS__), (1,1,0,0,0)\
+  )
+
+/**
+ * @todo Write documentation.
+ */
+#define STRING_FACADE_PARAMS_DECLARE_WITH_DEFAULTS()\
+  DETAIL_STRING_FACADE_PARAMS_ENUM(STRING_FACADE_PARAMS, (1,1,0,0,1))
+
+/**
+ * @todo Write documentation.
+ */
+#define STRING_FACADE_PARAMS_PASS(...)\
+  DETAIL_STRING_FACADE_PARAMS_ENUM(\
+    DETAIL_STRING_FACADE_PARAMS_REPLACE(((None, None))__VA_ARGS__), (0,1,0,0,0)\
+  )
+
+/**
+ * @todo Write documentation.
+ */
+#define STRING_FACADE_PARAMS_TYPEDEF()\
+  BOOST_PP_SEQ_FOR_EACH(\
+    DETAIL_STRING_FACADE_PARAM_TYPEDEF, _, STRING_FACADE_PARAMS\
+  )
+
+#endif // !defined STRING_FACADE_FWD_HPP
